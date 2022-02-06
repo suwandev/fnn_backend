0596029b62b37a9f452bd6629b4f21189a1ccbc0
suwanim <suwan.im@gmail.com>
2022-02-06 22:58:12 +0700

    update app js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
-app.post('/webhook', (req, res) => res.sendStatus(200))
+const port = process.env.PORT || 4000
+
+
+
+
+app.post('/webhook', (req, res) =>{
+    console.log('started')
+    res.sendStatus(200)} )
+    
+app.get('/',(req, res) =>{
+    console.log('started')
+    res.sendStatus(200)} )
+
+
 app.listen(port)
